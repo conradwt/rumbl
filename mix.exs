a6bde9199a3b129ebb789eaa764d2271513be80d
Conrad Taylor <conradwt@gmail.com>
2022-09-14 15:32:08 -0700

    Upgrade system requirements.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Rumbl.MixProject do
     [
       app: :rumbl,
       version: "0.1.0",
-      elixir: "~> 1.14.0-rc.0",
+      elixir: "~> 1.14.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
